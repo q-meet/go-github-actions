c734def0fc5409f08cf1131469417f773825b61c
15308850615 <1530885061@qq.com>
2021-08-11 23:16:37 +0800

    ok test run

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestW(t *testing.T) {
 	res := w()
-	if res != "Hello GitHub Actions. qikqiak.com is awesome12" {
+	if res != "Hello GitHub Actions. qikqiak.com is awesome" {
 		t.Errorf("Greet() = %s; Expected Hello GitHub Actions. qikqiak.com is awesome", res)
 	}
 }
