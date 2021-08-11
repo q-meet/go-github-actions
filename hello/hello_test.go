0373a95589691efd0711522ab9079a8f3061bd68
15308850615 <1530885061@qq.com>
2021-08-11 20:16:44 +0800

    ok

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreet(t *testing.T) {
 	result := Greet()
-	if result != "Hello GitHub Actions. qikqiak.com is awesome" {
+	if result != "Hello GitHub Actions. qikqiak.com is awesome122" {
 		t.Errorf("Greet() = %s; Expected Hello GitHub Actions. qikqiak.com is awesome", result)
 	}
 }
