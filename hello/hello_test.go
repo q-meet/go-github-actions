7fd1e92160b1615f6de7bc0c412e9c2cea81ffd3
15308850615 <1530885061@qq.com>
2021-08-11 13:56:32 +0800

    submit makefile

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestGreet(t *testing.T) {
 	result := Greet()
-	if result != "Hello GitHub Actions. qikqiak.com is awesome1" {
+	if result != "Hello GitHub Actions.1" {
 		t.Errorf("Greet() = %s; Expected Hello GitHub Actions. qikqiak.com is awesome", result)
 	}
 }
