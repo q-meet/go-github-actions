23f2c6c819da2da17ce525dca328acee5376153e
15308850615 <1530885061@qq.com>
2021-08-11 23:07:45 +0800

    ok two

--- main_test.go
+++ main_test.go
@@ -2,7 +2,7 @@ package main
 
 import "testing"
 
-func TestW(t testing.T) {
+func TestW(t *testing.T) {
 	res := w()
 	if res != "Hello GitHub Actions. qikqiak.com is awesome12" {
 		t.Errorf("Greet() = %s; Expected Hello GitHub Actions. qikqiak.com is awesome", res)
